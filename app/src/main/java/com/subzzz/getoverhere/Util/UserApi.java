df45b37c1cac97c2e2d5db7123aa57bf8f5616fd
Rafael Azriaiev <rafael.azrv@gmail.com>
2022-04-23 11:08:55 +0300

    updat UserApi

--- app/src/main/java/com/subzzz/getoverhere/Util/UserApi.java
+++ app/src/main/java/com/subzzz/getoverhere/Util/UserApi.java
@@ -7,6 +7,7 @@ import com.subzzz.getoverhere.Model.User;
 public class UserApi extends Application {
     private User currentUser;
     private String UType;
+    private String currentStatus;
     private static UserApi instance;
 
     public static UserApi getInstance(){
@@ -30,5 +31,13 @@ public class UserApi extends Application {
 
     public void setCurrentUser(User currentUser) {
         this.currentUser = currentUser;
+    }
+
+    public String getCurrentStatus() {
+        return currentStatus;
+    }
+
+    public void setCurrentStatus(String currentStatus) {
+        this.currentStatus = currentStatus;
     }
 }
